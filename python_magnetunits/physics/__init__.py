b740f1c34e04ccb2d03eeec73aa1885ba1246eeb
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2026-02-11 10:36:53 +0100

    add more physics

--- python_magnetunits/physics/__init__.py
+++ python_magnetunits/physics/__init__.py
@@ -1,9 +1,23 @@
 """
 Standard field definitions for scientific computing.
 
-This package provides pre-defined field collections organized by domain.
+This package provides pre-defined field collections organized by physics domain:
+
+- electromagnetic: Magnetic/electric fields, current, voltage, EM material properties
+- thermal: Temperature, heat flux, thermal material properties
+- hydraulics: Pressure, flow rate, velocity, fluid properties
+- mechanical: Stress, strain, displacement, mechanical material properties
 """
 
 from . import electromagnetic
+from . import thermal
+from . import hydraulics
+from . import mechanical
 
-__all__ = ["electromagnetic"]
+__all__ = [
+    "electromagnetic",
+    "thermal",
+    "hydraulics",
+    "mechanical",
+]
+
